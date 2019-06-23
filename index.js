39c1080c618040e2d3ea60effc8bf410eb4e6138
Joshua Chamberlain <JLCh4mberlain@gmail.com>
2019-06-23 11:52:15 +0100

    Fixed issue with GetElementByID

--- index.js
+++
@@ -1,6 +0,0 @@
-//To Stop the previous audio if another key is pressed
-window.addEventListener("play", function (evt) {
-    if (window.$_currentlyPlaying) {
-        window.$_currentlyPlaying.pause();
-    }
-}, true);
